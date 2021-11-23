ae1e3ab9e8a9625e9062d683e4af531c6c22780d
SusanaArzate <sarzateh@toluca.tecnm.mx>
2021-11-23 12:38:20 -0600

    Susi 12:38
    Eliminacion de carpeta

--- AuditorEscritor/includes/header.php
+++
@@ -1,3 +0,0 @@
-<div align="center" style="background-image: url('img/fondo_header.jpg'); background-size:cover; padding-top: 20px; padding-bottom: 20px;">
-    <h1 style="color:white">Agujeros Negros</h1>
-</div>
