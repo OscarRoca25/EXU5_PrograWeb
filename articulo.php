7bcbc354ec18f6910c18f05886c253feb7558ebb
SusanaArzate <sarzateh@toluca.tecnm.mx>
2021-11-24 14:06:02 -0600

    Susi 24/11/21
    Código terminado.

---
+++ articulo.php
@@ -0,0 +1,212 @@
+<?php
+$servidor = "bjmpkkoiv1c5rd7kgm65-mysql.services.clever-cloud.com";
+$usuarioBD = "uww4txp33fwtwsd3";
+$pdwBD = "SOScyNtal6c2jEyoSEAz";
+$nomBD = "bjmpkkoiv1c5rd7kgm65";
+$conn = mysqli_connect($servidor, $usuarioBD, $pdwBD, $nomBD);
+
+if (!$conn) {
+    echo 'Error de conexión: ' . mysqli_connect_error();
+}
+
+//Recuperamos id del articulo
+$id = $_GET['id'];
+
+//Recolectamos datos del articulo
+$sql = "SELECT * FROM articulos WHERE id_articulo = $id";
+
+$resultado = mysqli_query($conn, $sql);
+if ($resultado->num_rows == 1) {
+    session_start();
+    $articulo = mysqli_fetch_assoc($resultado);
+}
+
+$correo = $articulo["correo"];
+$titulo = $articulo["titulo"];
+$subtitulo = $articulo["subtitulo"];
+$fecha = $articulo["fecha_publicacion"];
+$categoria = $articulo["categoria"];
+$estatus_articulo = $articulo["estatus_articulo"];
+$contenido = $articulo["contenido"];
+
+//Recolectamos datos del autor del articulo
+$sql = "SELECT * FROM usuarios WHERE correo = '$correo'  ";
+
+$resultado = mysqli_query($conn, $sql);
+if ($resultado->num_rows == 1) {
+    $autor = mysqli_fetch_assoc($resultado);
+}
+
+$nombre = $autor["nombre"];
+$apellidos = $autor["apellidos"];
+
+//Recolectamos los comentarios del articulo
+$sql = "SELECT * FROM comentarios WHERE id_articulo = $id";
+$resultadoC = mysqli_query($conn,$sql);
+
+//Eliminación del articulo
+if (isset($_POST['submit'])) {
+    $sql = "UPDATE articulos SET estatus_articulo = 'eliminado' WHERE id_articulo = $id";
+
+    $resultado = mysqli_query($conn, $sql);
+    if ($resultado) {
+        header('Location: auditorEscritor.php');
+    } else {
+        echo "Error: " . $sql . ":" . mysqli_error($conn);
+    }
+}
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Articulo</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+    
+    
+</head>
+
+<body style="background-image: url('img/fondoG.jpeg'); background-size:cover; background-attachment:fixed">
+    <div class="container">
+
+        <?php
+        require_once 'includes/header.php';
+        ?>
+
+        <br>
+
+        <nav class="navbar justify-content" style="margin-top: -20px; background-color:rgb(14, 10, 20)">
+            <div class="navbar justify-content" style="color: white" id="navbarSupportedContent;" >
+                
+                <form class="form-inline" action="escritores.php">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit" style="color:white; border-color:purple">Escritores</button>
+                </form>
+
+                <form class="form-inline" action="auditorEscritor.php" style="margin-left:20px">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit" style="color:white; border-color:purple">Artículos</button>
+                </form>
+
+                <form class="form-inline my-2 my-lg-0" style="margin-left: 460px">
+                    <?php
+                    if (isset($_SESSION["nombre"])) {
+                        echo '<a class="nav-link btn btn-infoS" disabled><i class="fa fa-user" style="font-size: 25px;"></i>
+                         Auditor de Escritores</a>';
+                    }
+                    ?>
+                    
+                    <?php
+                    if (isset($_SESSION["nombre"])) {
+                        echo '<a href="includes/cerrarSesion.php" class="nav-link btn btn-danger" onclick="cerrarSesion()" style="margin-left: 15px; cursor: pointer; color:white; background-color:blue; border-color:blue"><i class="fa fa-sign-out-alt" style="font-size: 25px;"></i> Cerrar Sesión</a>';
+                    } else {
+                        echo '<a id="login" class="nav-link btn btn-primary" onclick="muestraForm()" style="margin-left: 15px; cursor: pointer;"><i class="fa fa-sign-in-alt" style="font-size: 25px;"></i> Iniciar Sesión</a>';
+                    }
+                    ?>
+
+                </form>
+            </div>
+        </nav>
+
+        <br>
+
+        <div class="card" style="background:linear-gradient(rgba(46, 17, 83,0.5),rgba(12, 67, 131,0.5))">
+            <div class="card-body" style="background:linear-gradient(rgba(46, 17, 83,0.3),rgba(12, 67, 131,0.3)); border-radius: 15px">
+                <br>
+                <div class="row" style="align:center; padding:0px">
+                    <div class="offset-md-2 col-md-8 offset-md-2">
+                        <!-- Categoria -->
+                        <h4 style="color: white; text-align: left;">| <?php echo $categoria?> |</h4>
+                        <!-- Titulo -->
+                        <h1 style="color: white; text-align: center;"><b><?php echo $titulo?></b></h1>
+                        <!-- Autor y fecha -->
+                        <p style="color: white; text-align:left">
+                            <pre><a href="escritor.php?correo=<?php echo $correo;?>">Por <?php echo $nombre?> <?php echo $apellidos?></a>                                       <a href="#"><?php echo "Fecha de publicación:"?> <?php echo $fecha?></a></pre>    
+                        </p>
+                        <!-- Separador -->
+                        <p>
+                            <pre style="color: white">___________________________________________________________________________________________</pre>    
+                        </p>
+                        <!-- Subtitulo -->
+                        <h5 style="color: white; text-align: left;"><?php echo $subtitulo?></h5>
+                        <!-- Contenido -->
+                        <br>
+                        <p style="color: white; text-align:justified">
+                            <?php echo $contenido?>
+                        </p>
+                        <br>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card-body" style="background:linear-gradient(rgba(46, 17, 83,0.3),rgba(12, 67, 131,0.3)); border-radius: 15px">
+                <br>
+                <div class="row" style="align:center; padding:0px">
+                    <div class="offset-md-2 col-md-8 offset-md-2">
+                        <h3 style="color: white; text-align: center">Comentarios</h3>
+                        <?php
+                        if ($resultadoC) {
+                            while ($comentarios = mysqli_fetch_array($resultadoC)) {
+                                //Datos del comentario
+                                $correoC = $comentarios["correo"];                                      
+                                $estatus_comentario = $comentarios["estatus_comentario"];
+                                $contenidoC = $comentarios["contenido"];
+                                
+                                if ($estatus_comentario == 'Publicado') {
+                                    //Datos del lector del comentario
+                                    $query = "SELECT * FROM usuarios WHERE correo = '$correoC'";
+                                    $res = mysqli_query($conn, $query);
+                                    if ($res->num_rows == 1) {
+                                        $lector = mysqli_fetch_assoc($res);
+                                    }
+                                    $nombreL = $lector["nombre"];
+                                    $apellidosL = $lector["apellidos"];
+                                    ?>
+                                    </br>
+                                    <p><pre style="color: white">___________________________________________________________________________________________</pre></p>
+                                    <p><pre  style="color: white; font-size:18px"><strong><?php echo $nombreL?> <?php echo $apellidosL?></strong> (<?php echo $correoC ?>)</pre></p>
+                                    <p style="color: white; padding:0px"><?php echo $contenidoC ?></p>
+                                    <p><pre style="color: white">___________________________________________________________________________________________</pre></p>
+                                    </br>
+                                    <?php
+                                }
+                            }
+                        } else {
+                            echo '<p>No hay comentarios</p>';
+                        }
+                        ?>
+                        <!-- Categoria -->
+                        
+                        <br>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <br>
+
+        <form class="form-inline" method="POST" style="margin-left:970px">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit" id="submit" name='submit' style="color:white; border-color:red; background-color:red">Eliminar artículo</button>
+        </form>
+
+        <br>
+
+        <?php
+        require_once 'includes/footer.php';
+        ?>
+
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ escritores.php
@@ -0,0 +1,128 @@
+<?php
+session_start();
+$servidor = "bjmpkkoiv1c5rd7kgm65-mysql.services.clever-cloud.com";
+$usuarioBD = "uww4txp33fwtwsd3";
+$pdwBD = "SOScyNtal6c2jEyoSEAz";
+$nomBD = "bjmpkkoiv1c5rd7kgm65";
+$db = mysqli_connect($servidor, $usuarioBD, $pdwBD, $nomBD);
+
+$query = "SELECT * FROM usuarios WHERE rol = 'escritor' AND estatus IN ('activo','suspendido')";
+$resultado = mysqli_query($db, $query);
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Escritores</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+</head>
+
+<body style="background-image: url('img/fondoG.jpeg'); background-size:cover; background-attachment:fixed">
+    <div class="container">
+
+        <?php
+        require_once 'includes/header.php';
+        ?>
+
+        <br>
+
+        <nav class="navbar justify-content" style="margin-top: -20px; background-color:rgb(14, 10, 20)">
+            <div class="navbar justify-content" style="color: white" id="navbarSupportedContent;" >
+                
+                <form class="form-inline">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit" style="color:white; border-color:purple; background-color:purple">Escritores</button>
+                </form>
+
+                <form class="form-inline" action="auditorEscritor.php" style="margin-left:20px">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit" style="color:white; border-color:purple">Artículos</button>
+                </form>
+
+                <form class="form-inline my-2 my-lg-0" style="margin-left: 460px">
+                    <?php
+                    if (isset($_SESSION["nombre"])) {
+                        echo '<a class="nav-link btn btn-infoS" disabled><i class="fa fa-user" style="font-size: 25px;"></i>
+                         Auditor de Escritores</a>';
+                    }
+                    ?>
+                    
+                    <?php
+                    if (isset($_SESSION["nombre"])) {
+                        echo '<a href="includes/cerrarSesion.php" class="nav-link btn btn-danger" onclick="cerrarSesion()" style="margin-left: 15px; cursor: pointer; color:white; background-color:blue; border-color:blue"><i class="fa fa-sign-out-alt" style="font-size: 25px;"></i> Cerrar Sesión</a>';
+                    } else {
+                        echo '<a id="login" class="nav-link btn btn-primary" onclick="muestraForm()" style="margin-left: 15px; cursor: pointer;"><i class="fa fa-sign-in-alt" style="font-size: 25px;"></i> Iniciar Sesión</a>';
+                    }
+                    ?>
+
+                </form>
+            </div>
+        </nav>
+
+        <br>
+
+        <div class="card" style="background:linear-gradient(rgba(46, 17, 83,0.5),rgba(12, 67, 131,0.5))">
+            <div class="card-body" style="background:linear-gradient(rgba(46, 17, 83, 0.5),rgba(12, 67, 131, 0.5)); border-radius: 15px;">
+                <br>
+                <div class="row" style="align:center">
+
+                    <div class="offset-md-2 col-md-8 offset-md-2">
+                        <h1 style="color: white; text-align: center;"><b>Escritores</b></h1>
+                        <hr>
+                        <table class="table table-dark table-striped table-bordered">
+                            <thead>
+                                <tr style="align:center">
+                                    <th>Nombre</th>
+                                    <th>Apellidos</th>
+                                    <th>Profesión</th>
+                                    <th>Perfil</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                if ($resultado) {
+                                    while ($mostrar = mysqli_fetch_array($resultado)) {
+                                        $correo = $mostrar["correo"];
+                                        $nombre = $mostrar["nombre"];
+                                        $apellidos = $mostrar["apellidos"];
+                                        $profesion = $mostrar["profesion"];
+                                        echo '<tr align="center">';
+                                        echo "<td>" . $nombre . "</td>";
+                                        echo "<td>" . $apellidos . "</td>";
+                                        echo "<td>" . $profesion . "</td>";
+                                        echo '<td><a href="escritor.php?correo=' . $correo . '" class="btn btn-info"><i class="fa fa-info-circle">Mostrar datos</i></a></td>';
+                                        echo '</tr>';
+                                    }
+                                } else {
+                                    echo '<tr align="center">';
+                                    echo '<td colspan="2">Tabla sin registros</td>';
+                                    echo '</td>';
+                                }
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <br>
+        
+        <?php
+        require_once 'includes/footer.php';
+        ?>
+
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
